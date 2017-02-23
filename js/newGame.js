4d8e5a66dfcf38710cb030cc54d6b4f3961a7abf
Fernando Flores <fflores10@uabc.edu.mx>
2017-02-23 14:58:09 -0800

    Added AssignTurns function

--- js/newGame.js
+++ js/newGame.js
@@ -1,3 +1,29 @@
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//               佛祖保佑         永无BUG
+// Esto bendecira nuestro código para que no tenga bugs
+
 function initGame() {
 	var numPlayers = document.getElementById('num_players').value;
 	if (numPlayers < 2 || numPlayers > 4) {
@@ -7,7 +33,7 @@ function initGame() {
 	var board = new Board();
 	board.shuffle();
 	var players = createPlayers(board, numPlayers);
-	console.log(players);
+	assignTurns(players);
 }
 
 function createPlayers(board, numPlayers) {
@@ -24,14 +50,29 @@ function createPlayers(board, numPlayers) {
 }
 
 function assignTurns(players) {
+	var maxdb = new Tile(0,0);
 	players.sort(function(player1, player2) {
 		var db1 = player1.doubles();
 		var db2 = player2.doubles();
 		if (db1.length) {
 			if (db2.length) {
-
-			}
+				maxdb1= (max(db1,maxTilePredicate));
+				maxdb2= (max(db2,maxTilePredicate));
+				console.log(maxdb1.x - maxdb2.x);
+				return maxdb1.x - maxdb2.x;
+				}
+			return 1;
+			} else if(db2.length){
+				return -1;
 		}
+		var max1 = max(player1.tiles,maxTilePredicate);
+		var max2 = max(player2.tiles,maxTilePredicate);
+		console.log(maxdb1.x - maxdb2.x);
+		return max1.x - max2.x;
+
+
+
+			
 		// TODO: This inner function should return -1 if player1 will play after player2,
 		//			1 if player1 will player before player2 or 0 if they have the same
 		//			ammount of points an no doubles
--- js/util.js
+++ js/util.js
@@ -11,5 +11,5 @@ function max(array, predicate) {
 };
 
 function maxTilePredicate(a, b) {
-	return a.x > b.x;
+	return (a.x + a.y) > (b.x + b.y);
 }
